c4f57e50c2fbca69b704552539261f0b733889d5
Lucas <Lucas.Sampaio@jala.university>
2025-01-16 11:33:49 -0300

    Inicio Finalizado

---
+++ 1 - Iniciante em Programação G8 - ONE/1 - Aprenda lógica de programação/1 - logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert("Bem vindo ao numero secreto");
+let numeroSecreto= 10;
+
+let numeroEscolhido = prompt("Digite o numero secreto entre 1 a 30");
+
+if(numeroSecreto == numeroEscolhido){
+    console.log("Acertou Miseravi! o número é " + numeroSecreto);
+} else {
+    alert("Errouuuu");
+}
+
---
+++ 1 - Iniciante em Programação G8 - ONE/1 - Aprenda lógica de programação/2 - desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,21 @@
+alert("Boas Vindas ao nosso site!");
+
+let nome = "Lua";
+let idade = "25";
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos")
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+alert(mensagemDeErro);
+
+nome = prompt("Digite seu nome!");
+idade = prompt("Agora digite sua idade!");
+
+if (idade >= 18){
+    alert("Pode tirar a habilitação!");
+} else {
+    alert("Ainda faltam "+ (18 - idade) + " anos para tirar a habilitação T_T");
+}
