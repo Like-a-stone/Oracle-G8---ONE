9d1037b72df30b6d19705b7351d259312c87b631
Lucas <Lucas.Sampaio@jala.university>
2025-01-16 23:00:16 -0300

    avancando com funcoes

---
+++ 1 - Iniciante em Programação G8 - ONE/2 - Lógica de programação: explore funções e listas/3 - desafio-js-projeto_final/logica-js-projeto_inicial/app.js
@@ -0,0 +1,34 @@
+// Criar uma função que exibe "Olá, mundo!" no console.
+
+function hello(){
+    console.log("Olá, mundo!");
+
+}
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+function helloName(name){
+    console.log(`Olá, ${name}!`);
+}
+
+//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+function doubleNumber(number){
+    return 2*number;
+}
+
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+function findMedian(number1, number2, number3){
+    return (number1 + number2 + number3) / 3;
+}
+
+//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+function findBiggester(number1, number2){
+    return number1 > number2 ? number1: number2;
+}
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+function multipleNumberByHimselft(number){
+    return number*number;
+}
